a7473a486c763e2acbcf8c94129e0a6acc647656
Ivan <ivan.serina@unibs.it>
2022-06-20 12:29:01 +0200

    test case update

--- test/test_up_lpg.py
+++ test/test_up_lpg.py
@@ -1,14 +1,17 @@
 import unified_planning
 from unified_planning.shortcuts import *
+from unified_planning.io.pddl_writer import PDDLWriter
+from unified_planning.io.pddl_reader import PDDLReader
 from unittest import TestCase
-
 
 class LPGtest(TestCase):
 
     def test_lpg(self):
         Location = UserType('Location')
-        robot_at = unified_planning.model.Fluent('robot_at', BoolType(), [Location])
-        connected = unified_planning.model.Fluent('connected', BoolType(), [Location, Location])
+
+        robot_at = unified_planning.model.Fluent('robot_at', BoolType(), l=Location)
+        connected = unified_planning.model.Fluent('connected', BoolType(), l_from=Location, l_to=Location)
+
         move = unified_planning.model.InstantaneousAction('move', l_from=Location, l_to=Location)
         l_from = move.parameter('l_from')
         l_to = move.parameter('l_to')
@@ -16,7 +19,8 @@ class LPGtest(TestCase):
         move.add_precondition(robot_at(l_from))
         move.add_effect(robot_at(l_from), False)
         move.add_effect(robot_at(l_to), True)
-
+        print(move)
+        
         problem = unified_planning.model.Problem('robot')
         problem.add_fluent(robot_at, default_initial_value=False)
         problem.add_fluent(connected, default_initial_value=False)
@@ -28,10 +32,15 @@ class LPGtest(TestCase):
 
         problem.set_initial_value(robot_at(locations[0]), True)
         for i in range(NLOC - 1):
-            problem.set_initial_value(connected(locations[i], locations[i + 1]), True)
+            problem.set_initial_value(connected(locations[i], locations[i+1]), True)
+
 
         problem.add_goal(robot_at(locations[-1]))
+        print(problem)       
 
         with OneshotPlanner(name='lpg') as planner:
-            plan = planner.solve(problem)
-            print("%s returned: %s" % (planner.name(), plan))
+            result = planner.solve(problem)
+            if result.status == up.engines.PlanGenerationResultStatus.SOLVED_SATISFICING:
+                print("Lpg returned: %s" % result.plan)
+            else:
+                print("No plan found.")
