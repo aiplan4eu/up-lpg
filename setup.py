99aaf6e34c143758b56ce711e68554aa81b7e87f
stoppini <rove.albe@gmail.com>
2023-01-02 12:42:25 +0100

    windows exec

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ arch = (platform.system(), platform.machine())
 
 EXECUTABLES = {
     ("Linux", "x86_64"): "lpg",
-    # ("Windows", "x86_64"): "aries_windows_x86_64.exe",
-    # ("Windows", "aarch64"): "aries_windows_aarch64.exe",
+    ("Windows", "x86_64"): "winlpg.exe",
+    ("Windows", "AMD64"): "winlpg.exe",
     # ("Windows", "x86"): "aries_windows_x86.exe",
     # ("Windows", "aarch32"): "aries_windows_aarch32.exe",
 }
@@ -39,7 +39,7 @@ class BinaryDistribution(Distribution):
         return False
 
 setup(name='up_lpg',
-      version='0.0.2.19',
+      version='0.0.2.20',
       description='up_lpg',
       long_description=long_description,
       long_description_content_type ="text/markdown",
