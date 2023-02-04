b9b19d6c856fe30f4d9d13dcd4adea7cea29ba2a
stoppini <rove.albe@gmail.com>
2023-02-04 12:56:06 +0100

    commit for new release with fixed planner

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ class BinaryDistribution(Distribution):
         return False
 
 setup(name='up_lpg',
-      version='0.0.4.2',
+      version='0.0.4.3',
       description='up_lpg',
       long_description=long_description,
       long_description_content_type ="text/markdown",
