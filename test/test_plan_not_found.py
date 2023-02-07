5199bd2e5265af21217d99a0054753a3c3bfb151
AfkValerik <v.borelli001@unibs.studenti.it>
2023-02-07 16:36:37 +0100

    added a test for the plan not found exception

---
+++ test/test_plan_not_found.py
@@ -0,0 +1,23 @@
+import unified_planning 
+from unified_planning.shortcuts import *
+from unified_planning.io.pddl_reader import PDDLReader
+from unified_planning.io.pddl_writer import PDDLWriter
+from unittest import TestCase
+
+
+
+reader = PDDLReader()
+domain_filename = sys.path[0] + "/pddl/sailing_domain.pddl"
+problem_filename = sys.path[0] + "/pddl/sailing_3_3_1229.pddl"
+
+problem = reader.parse_problem(domain_filename,problem_filename)
+
+
+
+
+with OneshotPlanner(name='lpg') as planner:
+            result = planner.solve(problem)
+            if result.status == up.engines.PlanGenerationResultStatus.SOLVED_SATISFICING:
+                print("Lpg returned: %s" % result.plan)
+            else:
+                print("No plan found.")
