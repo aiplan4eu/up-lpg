9db5a58113cce44d981fcaa99c6a4aa60f2144c5
Ivan <ivan.serina@unibs.it>
2024-03-09 13:11:04 +0100

    removed InfAction string from plan

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ class BinaryDistribution(Distribution):
         return False
 
 setup(name='up_lpg',
-      version='0.0.11',
+      version='0.0.12',
       description='up_lpg',
       long_description=long_description,
       long_description_content_type ="text/markdown",
